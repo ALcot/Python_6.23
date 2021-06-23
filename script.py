b975b91eaa8f06e3d744b339471fa78cc8cfd642
ALcot <mbaaccarima@gmail.com>
2021-06-23 22:13:09 +0900

    Add files via upload

---
+++ script.py
@@ -0,0 +1,14 @@
+# apple_price という変数に数値 200 を代入
+apple_price = 200
+
+# count という変数に数値 5 を代入
+count = 5
+
+# total_price という変数に、 apple_price と count を掛けたものを代入
+total_price = apple_price * count
+
+# 「 購入するりんごの個数は○○個です 」となるように出力
+print('購入するりんごの個数は' + str(count) + '個です')
+
+# 「 支払い金額は○○円です 」となるように出力
+print('支払い金額は' + str(total_price) + '円です')
